d02ff05289b77838c4b29898971fb571f87be733
Florian <unzico@gmail.com>
2019-06-21 07:53:40 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import ChatBot from "react-simple-chatbot";
+import "./App.css";
+
+const steps = [
+  {
+    id: "0",
+    message: "Welcome to react chatbot!",
+    trigger: "1"
+  },
+  {
+    id: "1",
+    message: "Bye!",
+    end: true
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <ChatBot steps={steps} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
